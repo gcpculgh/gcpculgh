974da29fa0cfbdb85418885f7d52d64a3d94ac3e
gcpculgh <infolinkghana@gmail.com>
2026-08-28 18:37:28 +0000

    Update ALLOWED_HOSTS and TEMPLATES DIRS for production

--- gcpcul/gcpcul/settings.py
+++ gcpcul/gcpcul/settings.py
@@ -58,13 +58,18 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'gcpcul.urls'
 
+import os
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
