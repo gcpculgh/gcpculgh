d188cae3c03315abe8500d7ba0c3b7504b6fa8ad
gcpculgh <infolinkghana@gmail.com>
2026-09-01 12:06:28 +0000

    Updated the ABOUT-US page

--- gcpcul/gcpcul/settings.py
+++ gcpcul/gcpcul/settings.py
@@ -23,11 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-nik#-^m9%f$l+@-70v9$qu04*uexuv!z3)9$&%j4b^%i+uo$t+'
 
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['gcpculgh.com', 'www.gcpculgh.com', '.vercel.app']
 ALLOWED_HOSTS = ["*"]
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
