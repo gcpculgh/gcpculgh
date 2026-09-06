e9c630f7db20b1f4f6e7eaa0ce47f3f131f7099d
gcpculgh <infolinkghana@gmail.com>
2026-09-06 15:53:14 +0000

    Updated various inconsistent icons, typography and colors

--- gcpcul/gcpcul/settings.py
+++ gcpcul/gcpcul/settings.py
@@ -24,8 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nik#-^m9%f$l+@-70v9$qu04*uexuv!z3)9$&%j4b^%i+uo$t+'
 
 DEBUG = False
+# DEBUG = True
 
 ALLOWED_HOSTS = ['gcpculgh.com', 'www.gcpculgh.com', '.vercel.app']
+# ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
